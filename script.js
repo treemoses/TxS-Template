2c52adad374b55c150e67d5082590e91beac6c88
Radames Ajna <radamajna@gmail.com>
2018-08-08 15:56:50 -0700

    more styles

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// drake album picture https://i.scdn.co/image/012ecd119617ac24ab56620ace4b81735b172686
+//album URI
+// songURI spotify:track:2G7V7zsVDxg1yRsu7Ew9RJ
+
 const gifs = [
   'https://i.giphy.com/media/BCXMSiVZeo8xy/giphy.gif',
   'https://i.giphy.com/media/1d5KHhOA1oTpX7ROOi/giphy.gif',
@@ -11,15 +15,28 @@ const gifs = [
   'https://i.giphy.com/media/xe9csf50g4SqY/giphy.gif',
   'https://i.giphy.com/media/l0HlOWpCtvk3ImJpe/giphy.gif'
 ]
-
+// spotify:album:7yf9ZJyHRbp8sHtpDKfPpt
 document.querySelector('#range').addEventListener('change', (e) => {
   console.log(e.target.value)
   document.querySelector('.score').innerHTML = e.target.value / 10
   document.querySelector('.box-image').style.backgroundImage = `url(${gifs[Math.floor((e.target.value)/10)]})`;
 })
 
+console.log(getAlbumCover('spotify:album:3X8Nrq4TvSd54ymTiXYcD6'))
+
+
+
 
 function getSpotifySrc(song) {
   const songCode = song.split(':')[2];
   return `https://open.spotify.com/embed/track/${songCode}`;
+}
+
+
+function getAlbumCover(song) {
+  //return the Image for a specific song ID
+  const songCode = song.split(':')[2];
+  const data = fetch(`https://cors-anywhere.herokuapp.com/https://embed.spotify.com/oembed?url=http://open.spotify.com/track/${songCode}`).
+                     then(r => r.json());
+  return data;
 }
