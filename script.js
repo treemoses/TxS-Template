c1c3cda59bdb91de8fe1c8de48cd0523a80d5199
treemoses <teatreemoses@gmail.com>
2018-08-14 16:34:17 -0700

    created lists and class functions

--- script.js
+++ script.js
@@ -18,7 +18,7 @@
 
 var resultBox = document.getElementById('result')
 var titleBox = document.getElementById('title')
-var voteBox = document.getElementById('vote-box')
+var gifPanel = document.getElementById('gif-panel')
 var mainImage = document.getElementById('main-image')
 var youImage = document.getElementById('you-image')
 var spotifyImage = document.getElementById('spotify-image')
@@ -26,17 +26,100 @@ var yrImage = document.getElementById('yr-image')
 var youScore = document.getElementById('you-score')
 var spotifyScore = document.getElementById('spotify-score')
 var yrScore = document.getElementById('yr-score')
-
+var userRating = document.getElementById('clickRating')
+var compareButton = document.getElementById('result-button')
 // this is how you replace the image from the background
 mainImage.style.backgroundImage = "url(https://i.giphy.com/media/1d5KHhOA1oTpX7ROOi/giphy.gif)"
 
 // this is how we hide the resultBox element
 resultBox.hidden = true;
+compareButton.hidden = true;
 
 // this is how you create a function
 function testClickFunction(){
   alert('clickedddd')
 }
+function showResultBox(){
+
+}
+var userRating;
+userRating = 1
+userRating = 2
+userRating = 3
+userRating = 4
+userRating = 5
+userRating = 6
+userRating = 7
+userRating = 8
+userRating = 9
+
+function clicked1() {
+  showButton()
+  userRating = 1;
+  mainImage.style.backgroundImage = "url(https://i.giphy.com/media/BCXMSiVZeo8xy/giphy.gif)";
+}
+function clicked2() {
+  showButton()
+  userRating = 2;
+  mainImage.style.backgroundImage = "url(https://i.giphy.com/media/1d5KHhOA1oTpX7ROOi/giphy.gif)";
+}
+function clicked3() {
+  showButton()
+  userRating = 3;
+  mainImage.style.backgroundImage = "url(https://i.giphy.com/media/3o6Zt5nSYQa0czSmnm/giphy.gif)";
+}
+function clicked4() {
+  showButton()
+  userRating = 4;
+  mainImage.style.backgroundImage = "url(https://i.giphy.com/media/2tKCVxXIkBgqUZGDJb/giphy.gif)";
+}
+function clicked5() {
+  showButton()
+  userRating = 5;
+  mainImage.style.backgroundImage = "url(https://i.giphy.com/media/KtfjlVSFI8EVO/giphy.gif)";
+}
+function clicked6() {
+  showButton()
+  userRating = 6;
+  mainImage.style.backgroundImage = "url(https://i.giphy.com/media/xUPGGDgB1ZlUduRov6/giphy.gif)";
+}
+function clicked7() {
+  showButton()
+  userRating = 7;
+  mainImage.style.backgroundImage = "url(https://i.giphy.com/media/wAxlCmeX1ri1y/giphy.gif)";
+}
+function clicked8() {
+  showButton()
+  userRating = 8;
+  mainImage.style.backgroundImage = "url(https://i.giphy.com/media/MhHcCN6PoTdL2/giphy.gif)";
+}
+function clicked9() {
+  showButton()
+  userRating = 9;
+  mainImage.style.backgroundImage = "url(https://i.giphy.com/media/1TJB4TPjtaEJq/giphy.gif)";
+}
+function clicked10() {
+  showButton()
+  userRating = 10;
+  mainImage.style.backgroundImage = "url(https://i.giphy.com/media/xe9csf50g4SqY/giphy.gif)";
+}
+
+function clickedCompare(){
+  resultBox.hidden = false;
+  gifPanel.hidden = true;
+  youScore.innerHTML = userRating;
+}
+
+function clickRating(){
+  voteBox.hidden = true;
+}
+
+function showButton(){
+  compareButton.hidden = false;
+}
+
+
+
 
 // Activity today will be
 
